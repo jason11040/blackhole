9bf410b40cb695ab7be7cfe5592bee99db068204
Jason11040 <xxjokerxx34@hotmail.com>
2022-04-15 23:57:35 -0500

    Initial commit

---
+++ code/Game.cs
@@ -0,0 +1,151 @@
+﻿using Sandbox;
+using System.Linq;
+using System.Threading.Tasks;
+
+partial class SandboxGame : Game
+{
+	public SandboxGame()
+	{
+		if ( IsServer )
+		{
+			// Create the HUD
+			_ = new SandboxHud();
+		}
+	}
+
+	public override void ClientJoined( Client cl )
+	{
+		base.ClientJoined( cl );
+		var player = new SandboxPlayer( cl );
+		player.Respawn();
+
+		cl.Pawn = player;
+	}
+
+	protected override void OnDestroy()
+	{
+		base.OnDestroy();
+	}
+
+	[ServerCmd( "spawn" )]
+	public static async Task Spawn( string modelname )
+	{
+		var owner = ConsoleSystem.Caller?.Pawn;
+
+		if ( ConsoleSystem.Caller == null )
+			return;
+
+		var tr = Trace.Ray( owner.EyePosition, owner.EyePosition + owner.EyeRotation.Forward * 500 )
+			.UseHitboxes()
+			.Ignore( owner )
+			.Run();
+
+		var modelRotation = Rotation.From( new Angles( 0, owner.EyeRotation.Angles().yaw, 0 ) ) * Rotation.FromAxis( Vector3.Up, 180 );
+
+		//
+		// Does this look like a package?
+		//
+		if ( modelname.Count( x => x == '.' ) == 1 && !modelname.EndsWith( ".vmdl", System.StringComparison.OrdinalIgnoreCase ) && !modelname.EndsWith( ".vmdl_c", System.StringComparison.OrdinalIgnoreCase ) )
+		{
+			modelname = await SpawnPackageModel( modelname, tr.EndPosition, modelRotation, owner );
+			if ( modelname == null )
+				return;
+		}
+
+		var model = Model.Load( modelname );
+		if ( model == null || model.IsError )
+			return;
+
+		var ent = new Prop
+		{
+			Position = tr.EndPosition + Vector3.Down * model.PhysicsBounds.Mins.z,
+			Rotation = modelRotation,
+			Model = model
+		};
+
+		// Let's make sure physics are ready to go instead of waiting
+		ent.SetupPhysicsFromModel( PhysicsMotionType.Dynamic );
+
+		// If there's no physics model, create a simple OBB
+		if ( !ent.PhysicsBody.IsValid() )
+		{
+			ent.SetupPhysicsFromOBB( PhysicsMotionType.Dynamic, ent.CollisionBounds.Mins, ent.CollisionBounds.Maxs );
+		}
+	}
+
+	static async Task<string> SpawnPackageModel( string packageName, Vector3 pos, Rotation rotation, Entity source )
+	{
+		var package = await Package.Fetch( packageName, false );
+		if ( package == null || package.PackageType != Package.Type.Model || package.Revision == null )
+		{
+			// spawn error particles
+			return null;
+		}
+
+		if ( !source.IsValid ) return null; // source entity died or disconnected or something
+
+		var model = package.GetMeta( "PrimaryAsset", "models/dev/error.vmdl" );
+		var mins = package.GetMeta( "RenderMins", Vector3.Zero );
+		var maxs = package.GetMeta( "RenderMaxs", Vector3.Zero );
+
+		// downloads if not downloads, mounts if not mounted
+		await package.MountAsync();
+
+		return model;
+	}
+
+	[ServerCmd( "spawn_entity" )]
+	public static void SpawnEntity( string entName )
+	{
+		var owner = ConsoleSystem.Caller.Pawn as Player;
+
+		if ( owner == null )
+			return;
+
+		var attribute = Library.GetAttribute( entName );
+
+		if ( attribute == null || !attribute.Spawnable )
+			return;
+
+		var tr = Trace.Ray( owner.EyePosition, owner.EyePosition + owner.EyeRotation.Forward * 200 )
+			.UseHitboxes()
+			.Ignore( owner )
+			.Size( 2 )
+			.Run();
+
+		var ent = Library.Create<Entity>( entName );
+		if ( ent is BaseCarriable && owner.Inventory != null )
+		{
+			if ( owner.Inventory.Add( ent, true ) )
+				return;
+		}
+
+		ent.Position = tr.EndPosition;
+		ent.Rotation = Rotation.From( new Angles( 0, owner.EyeRotation.Angles().yaw, 0 ) );
+
+		//Log.Info( $"ent: {ent}" );
+	}
+
+	public override void DoPlayerNoclip( Client player )
+	{
+		if ( player.Pawn is Player basePlayer )
+		{
+			if ( basePlayer.DevController is NoclipController )
+			{
+				Log.Info( "Noclip Mode Off" );
+				basePlayer.DevController = null;
+			}
+			else
+			{
+				Log.Info( "Noclip Mode On" );
+				basePlayer.DevController = new NoclipController();
+			}
+		}
+	}
+
+	[AdminCmd( "respawn_entities" )]
+	public static void RespawnEntities()
+	{
+		Map.Reset( DefaultCleanupFilter );
+	}
+}
---
+++ code/entities/BlackHoleEnt.cs
@@ -0,0 +1,84 @@
+﻿using Sandbox;
+using System;
+using System.Linq;
+
+[Library( "ent_blackholeent", Title = "Black Hole", Spawnable = true )]
+public partial class BlackHoleEnt : ModelEntity
+{
+	public float PullRadius { get; set; } = 40000f;
+	public float DeathRadius { get; set; } = 1100f;
+	public float PullSpeed { get; set; }
+	public int Gravity { get; set; }
+
+	public override async void Spawn()
+	{
+		base.Spawn();
+		DeleteOthers();
+		SetModel( "models/ball/ball.vmdl" );
+		SetupPhysicsFromModel( PhysicsMotionType.Static, false );
+		Scale = 100f;
+		RenderColor = Color.Black;
+		while ( PullSpeed < 100f )
+		{
+			await GameTask.DelaySeconds( 1 );
+			PullSpeed += 0.001f;
+			Gravity = (int)(PullSpeed * 1000);
+			DebugOverlay.ScreenText( "Gravity Speed: " + Gravity.ToString(), 1f );
+		}
+	}
+
+	[Event.Tick]
+	public override void Simulate( Client cl )
+	{
+		//Log.Info( PullSpeed );
+		var nearEnts = Entity.FindInSphere( Position, PullRadius );
+		{
+			//DebugOverlay.Sphere( Position, PullRadius, Color.Red );
+			foreach ( var ent in nearEnts )
+			{
+				if ( ent is Player )
+				{
+					ent.Position = Vector3.Lerp( ent.Position, Transform.Position, PullSpeed * Time.Delta );
+				}
+				if ( Gravity >= 50f )
+				{
+					Log.Info( "Pull harder" );
+					ent.Position = Vector3.Lerp( ent.Position, Transform.Position, PullSpeed * Time.Delta * 2 );
+				}
+				/*if ( ent.GroundEntity == null )
+				{
+					Log.Info( "Grounded: " + (ent.GroundEntity != null) );
+					ent.Position = Vector3.Lerp( ent.Position, Transform.Position, PullSpeed * Time.Delta * 2 );
+				}*/
+			}
+
+			var nearDeath = Entity.FindInSphere( Position, DeathRadius );
+			{
+				//DebugOverlay.Sphere( Position, DeathRadius, Color.Blue );
+				foreach ( var ent in nearDeath )
+				{
+					if ( ent is Player )
+					{
+						ent.TakeDamage( DamageInfo.Generic( 1f ) );
+					}
+				}
+			}
+		}
+	}
+	public override void Touch( Entity other )
+	{
+		var player = other as Player;
+		//Log.Info( "Touched" );
+		other.TakeDamage( DamageInfo.Generic(5f));
+	}
+	private void DeleteOthers()
+	{
+		// Only allow one of these to be spawned at a time
+		foreach ( var ent in All.OfType<BlackHoleEnt>()
+			.Where( x => x.IsValid() && x != this ) )
+		{
+			PullSpeed = 0;
+			ent.Delete();
+		}
+	}
+}
